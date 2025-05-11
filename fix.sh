81ece9c751ff277028150318e8d458ad3aff8aec
Karsten <kmue@me.com>
2025-05-11 16:25:56 +0200

    moved apsell and mogrify

---
+++ fix.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Remove EXIF Information From Images
+find static/images -type "f" -exec mogrify -strip {} \;
+
+## Spellchecker
+# echo sailing | aspell --lang=en create master "$(pwd)"/aspell.en.dict
+# echo Segeln | aspell --lang=en create master "$(pwd)"/aspell.de.dict
+
+for lang in de en; do
+    find site/content/ -type "f" -name "*.$lang.md" -exec aspell --mode=markdown --lang=$lang --extra-dicts="$(pwd)"/aspell.$lang.dict check {} \;
+done
--- run.sh
+++ run.sh
@@ -5,11 +5,4 @@ trap "rm -rf public resources/_gen" EXIT
 cd site || exit 1
 rm -rf public resources/_gen
 
-## Remove EXIF Information From Images
-find static/images -type "f" -exec mogrify -strip {} \;
-
-## Spellchecker
-cwd=$(pwd)
-find content/posts/ -type "f" -name "*.md" -exec aspell --mode=markdown --lang=en --extra-dicts="$cwd"/aspell.dict check {} \;
-
 hugo server --disableFastRender
