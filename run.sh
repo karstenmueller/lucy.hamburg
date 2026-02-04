dd0133039aa5e91cc64328b8917e59723eebba9e
Karsten <kmue@me.com>
2026-02-04 18:13:03 +0100

    workflow cloudflare

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+set -ex
 trap "rm -rf public resources/_gen" EXIT
+
+source .env
+
+curl -L -o /tmp/hugo.pkg  https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_darwin-universal.pkg
+
+sudo installer -pkg /tmp/hugo.pkg -target /
+hugo version
 
 cd site || exit 1
 rm -rf public resources/_gen
