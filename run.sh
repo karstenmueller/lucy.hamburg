50021bc57ff0d3eaa2ec6f0af76102ae227f1f7d
Karsten <kmue@me.com>
2026-02-08 00:21:06 +0100

    fix

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ update() {
     curl -L -o /tmp/hugo.pkg  https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_darwin-universal.pkg
     sudo installer -pkg /tmp/hugo.pkg -target /
     hugo version
-    hugo mod get -u
 }
 
 source .env
 
 cd site || exit 1
 rm -rf public resources/_gen
+hugo mod get -u
 
 hugo server --watch --disableFastRender --cleanDestinationDir --forceSyncStatic
